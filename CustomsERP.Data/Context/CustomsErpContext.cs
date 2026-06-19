1ec18b177fb6af8e1ee07d67559460f5ca5b7d07
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-06-19 13:28:11 +0300

    Database migrations setup

---
+++ CustomsERP.Data/Context/CustomsErpContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using CustomsERP.Core;
+
+namespace CustomsERP.Data.Context;
+
+public class CustomsErpContext : DbContext
+{
+    public DbSet<Shipment> Shipments { get; set; }
+    public DbSet<Document> Documents { get; set; }
+    public DbSet<Exporter> Exporters { get; set; }
+    public DbSet<Port> Ports { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductVariety> ProductVarieties { get; set; }
+    public DbSet<Receiver> Receivers { get; set; }
+    public DbSet<ShippingCompany> ShippingCompanies { get; set; }
+    public DbSet<Warehouse> Warehouses { get; set; }
+    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=CustomERPData.db");
+    }
+    
+    //rules for making things like eori/Wh/port/product etc. codes correct
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Exporter>().HasIndex(e => e.Eori).IsUnique();
+        modelBuilder.Entity<Port>().HasIndex(p => p.PortCode).IsUnique();
+        modelBuilder.Entity<Product>().HasIndex(p => p.HsCode).IsUnique();
+        modelBuilder.Entity<Receiver>().HasIndex(r => r.Eori).IsUnique();
+        modelBuilder.Entity<Warehouse>().HasIndex(w => w.WarehouseCode).IsUnique();
+    }
+}
