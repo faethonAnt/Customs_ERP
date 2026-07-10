8cc43ae2b67e200406779cad65f9141d0aedf2bb
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-07-10 16:12:05 +0300

    Starting Model Annotations with Warehouse model

--- CustomsERP.Core/Models/Warehouse.cs
+++ CustomsERP.Core/Models/Warehouse.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CustomsERP.Core;
 
 public class Warehouse
 {
     public int Id { get; set; }
+    
+    [Required]
+    [StringLength(16)]
+    [RegularExpression(@"^GR\d{14}$")]
     public string WarehouseCode { get; set; }
 }
