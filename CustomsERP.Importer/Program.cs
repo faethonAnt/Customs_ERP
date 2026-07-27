ecce74769a4265bb7f9b7cc3e855b9d3fa500f04
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-07-27 12:56:59 +0300

    Helper script to extract products from csv files directly into the database

--- CustomsERP.Importer/Program.cs
+++ CustomsERP.Importer/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CustomsERP.Core;
+using CustomsERP.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+var optionsBuilder = new DbContextOptionsBuilder<CustomsErpContext>();
+optionsBuilder.UseSqlite("Data Source=../CustomsERP.Data/CustomERPData.db");
+
+using var context = new CustomsErpContext(optionsBuilder.Options);
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    HasHeaderRecord = false
+};
+
+using var reader = new StreamReader("products.csv");
+using var csv = new CsvReader(reader, config);
+
+var seenCodes = new HashSet<string>();
+var productsToAdd = new List<Product>();
+
+while (csv.Read())
+{
+    var rawCode = csv.GetField(0);
+    var description = csv.GetField(4);
+
+    var code = rawCode.Split(' ')[0];
+    if (!seenCodes.Add(code)) continue;
+    
+    productsToAdd.Add(new Product{HsCode = code, Name = description});
+}
+
+context.Products.AddRange(productsToAdd);
+context.SaveChanges();
+
+Console.WriteLine($"Imported {productsToAdd.Count} products.");
