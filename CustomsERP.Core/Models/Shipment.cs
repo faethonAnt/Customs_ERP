6f692be9bf7dc91ce4d92bf47493923cccbb91b2
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-08-17 16:00:55 +0300

    shipment file updated so the fields become lists

--- CustomsERP.Core/Models/Shipment.cs
+++ CustomsERP.Core/Models/Shipment.cs
@@ -12,22 +12,22 @@ public class Shipment
     
     //Exporter reference
     public int ExporterId { get; set; }
-    public Exporter Exporter { get; set; }
-    
+    public Exporter? Exporter { get; set; }
+
     //Receiver reference
     public int ReceiverId { get; set; }
-    public Receiver Receiver { get; set; }
-    
+    public Receiver? Receiver { get; set; }
+
     //Shipping Company reference
     public int ShippingCompanyId { get; set; }
-    public ShippingCompany ShippingCompany { get; set; }
-    
-    
+    public ShippingCompany? ShippingCompany { get; set; }
+
+
     public int PortId { get; set; }
-    public Port Port { get; set; }
-    
+    public Port? Port { get; set; }
+
     public int WarehouseId { get; set; }
-    public Warehouse Warehouse { get; set; }
+    public Warehouse? Warehouse { get; set; }
     
     public List<ProductVariety> ProductVarieties { get; set; } = new();
     public List<Document> Documents { get; set; }= new();
